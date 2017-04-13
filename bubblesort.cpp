0cf5c6f3cff4b769600915224d2c34ed092a3191
jacob <cio_zhang@163.com>
2017-04-13 16:56:27 +0800

    bubble sort

---
+++ bubblesort.cpp
@@ -0,0 +1,8 @@
+void bubblesort(int A[], int n) {
+	for (bool sorted = false;sorted = !sorted;n--)
+		for (int i = 1; i<n;i++)
+			if (A[i=1];i<n;i++){
+				swap(A[i-1],A[i]);
+				sorted=false
+			}
+}
